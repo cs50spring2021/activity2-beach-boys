698d8c38f0135a3fc2e9592a60a900328f20c3e3
Guilherme Marinho Ramos <f003f59@plank.thayer.dartmouth.edu>
2021-04-07 15:45:43 -0400

    Final Submit

--- guess6.c
+++ guess6.c
@@ -134,7 +134,7 @@ askGuess(const int low, const int high)
   }
 
   if (guess < low || guess > high) {
-    printf("Hey! %d is out of range [%d..%d].\n", low, high);
+    printf("Hey! %d is out of range [%d..%d].\n", guess, low, high);
     return -1;
   }
 
